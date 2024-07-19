e81040d18ab6ec7c61fe92a103b5636aae598d61
Brendan Keaton <BrendanRKeaton@gmail.com>
2024-07-19 16:01:56 -0400

    fix: ga4

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { useEffect } from "react";
-import { useRouter } from "next/router";
 import ReactGA from "react-ga4";
 import Hero from "./hero/hero";
 import About from "./components/about";
@@ -9,23 +8,11 @@ import Projects from "./projects/projects";
 import Experiences from "./experiences/experiences";
 
 export default function Home() {
-  const router = useRouter();
-
   useEffect(() => {
     ReactGA.initialize("G-05YDKDM7HY");
 
     ReactGA.send({ hitType: "pageview", page: window.location.pathname });
-
-    const handleRouteChange = (url: string) => {
-      ReactGA.send({ hitType: "pageview", page: url });
-    };
-
-    router.events.on("routeChangeComplete", handleRouteChange);
-
-    return () => {
-      router.events.off("routeChangeComplete", handleRouteChange);
-    };
-  }, [router.events]);
+  });
 
   return (
     <div className="pb-48 max-w-full overflow-x-hidden">
