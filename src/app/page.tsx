1cd4694df204ff418fffd9a98409d420ca2516a3
Brendan Keaton <BrendanRKeaton@gmail.com>
2024-10-07 23:50:10 -0400

    remove analytics

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,11 +8,6 @@ import Projects from "./projects/projects";
 import Experiences from "./experiences/experiences";
 
 export default function Home() {
-  useEffect(() => {
-    ReactGA.initialize("G-05YDKDM7HY");
-
-    ReactGA.send({ hitType: "pageview", page: window.location.pathname });
-  });
 
   return (
     <div className="pb-48 max-w-full overflow-x-hidden">
